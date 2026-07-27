4b7eea89e53295b2036d338771649a4b55d85574
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-07-27 10:43:14 +0530

    0075-SORT-COLORS Stats: Time: 0 ms (100%), Space: 43.2 MB (95.47%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,20 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        int low = 0, mid = 0, high = n - 1;
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                int temp1 = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp1;
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
                 low++;
                 mid++;
-            } else if (nums[mid] == 1) {
+            }
+            else if(nums[mid]==1){
                 mid++;
-            } else { 
-                int temp2 = nums[mid];
-                nums[mid] = nums[high];
-                nums[high] = temp2;
+            }
+            else{
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
                 high--;
             }
         }
