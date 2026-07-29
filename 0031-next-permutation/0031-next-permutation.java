c998c5b91df92a5d154baf034c7ede39f6017090
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-07-29 12:11:25 +0530

    0031-NEXT-PERMUTATION Stats: Time: 0 ms (100%), Space: 44.9 MB (16.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -19,20 +19,18 @@ class Solution {
     public int[] nextPermutation(int[] nums) {
         int ind = -1;
         int n = nums.length;
-        
-        for(int i=n-2;i>=0;i--){
-            if(nums[i] < nums[i+1]){
+        for(int i = n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
                 ind = i;
                 break;
             }
         }
-
-        if (ind ==-1){
+        if(ind == -1){
             reverse(nums,0,n-1);
             return nums;
         }
 
-        for(int i = n-1;i>ind;i--){
+        for(int i = n-1;i>=ind;i--){
             if(nums[i]>nums[ind]){
                 int temp = nums[i];
                 nums[i] = nums[ind];
@@ -40,8 +38,8 @@ class Solution {
                 break;
             }
         }
-
-        reverse(nums,0+ind+1, n-1);
+        reverse(nums, ind+1,n-1);
         return nums;
+        
     }
 }
