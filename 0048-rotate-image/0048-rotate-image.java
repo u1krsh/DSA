d679dd4075049d3d879e475655b19196b8fc6e2f
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-07-31 13:08:36 +0530

    0048-ROTATE-IMAGE Stats: Time: 0 ms (100%), Space: 43.7 MB (71.96%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -19,15 +19,13 @@ static void reverse(int[][] array) {
 
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j = i+1;j<n;j++){
+        for(int i =0;i<n;i++){
+            for (int j = i+1;j<n;j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
-
-    reverse(matrix);
-    
+        reverse(matrix);
     }
 }
