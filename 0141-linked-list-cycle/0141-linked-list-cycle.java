a54f378bb268753c6c8b39f206309dd04b20f444
Uttkarsh Singh <72338896+u1krsh@users.noreply.github.com>
2026-09-03 11:10:03 +0530

    0141-LINKED-LIST-CYCLE Stats: Time: 0 ms (100%), Space: 46.9 MB (33.64%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,15 +13,12 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode hare = head;
         ListNode turtle = head;
-
         while(hare != null && hare.next != null){
             turtle = turtle.next;
             hare = hare.next.next;
-            if(hare==turtle){
-                return true;
-            }
-            
+            if(hare == turtle) return true;
         }
+
         return false;
     }
 }
